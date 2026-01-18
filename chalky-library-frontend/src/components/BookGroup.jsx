67422bc1d54869390ff9f58222c71f8638bdcc37
DenizShayan <92824050+DenizShayan@users.noreply.github.com>
2026-01-17 23:18:34 -0500

    Update BookGroup.jsx

--- chalky-library-frontend/src/components/BookGroup.jsx
+++ chalky-library-frontend/src/components/BookGroup.jsx
@@ -1,16 +1,16 @@
-import BookCard from './BookCard';
+import BookCard from "./BookCard";
 
 // Displays a group of books with a title
 export default function BookGroup({ title, books }) {
-    return (
-        <section class="mb-6" >
-            <h2 className="text-lg font-semibold mb-3">{title}</h2>
+  return (
+    <section className="mb-6">
+      <h2 className="text-lg font-semibold mb-3">{title}</h2>
 
-            <div className=" gap-4 overflow-x-auto">
-                {books.map((book) => (
-                    <BookCard key={book.id} book={book} />
-                ))}
-            </div>
-        </section>
-    );
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+        {books.map((book) => (
+          <BookCard key={book.id} book={book} />
+        ))}
+      </div>
+    </section>
+  );
 }
